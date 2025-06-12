9051c56e60d638a271e8f0b5fc04dfb44f45c48c
Marcos Vinicius Silva <mrcsvs@outlook.com>
2025-06-12 16:49:26 -0300

    OCP Refactoring

---
+++ src/ocp/main.ts
@@ -0,0 +1,27 @@
+// Open/Closed -> Abertas para extensão, mas fechadas para modificação.
+
+import { Messaging } from "./services/messaging";
+import { Order } from "./classes/order";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/product";
+import { ShoppingCart } from "./classes/shopping-cart";
+import { NoDiscount } from "./classes/discount";
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product("Camiseta", 49.0));
+shoppingCart.addItem(new Product("Caderno", 9.0));
+shoppingCart.addItem(new Product("Lápis", 2.0));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
